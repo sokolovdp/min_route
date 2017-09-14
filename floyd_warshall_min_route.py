eadabab0ca98cc8a1025c8e310e3d190cf76c595
Dmitrii Sokolov <sokolovdp@gmail.com>
2017-09-15 00:22:57 +0300

    floyd warshall algorithm  to find shortest path

---
+++ floyd_warshall_min_route.py
@@ -0,0 +1,93 @@
+# Floyd - Warshall shortest route algorithm
+import numpy as np
+
+MAXIMAL = 10000
+MINIMAL = -1
+
+
+def create_adjacency_matrix(distance_data: "list") -> "np.ndarray":
+    n = len(distance_data)
+    m = len(distance_data[0])
+    matrix_size = n * m
+    adjacency_matrix = np.full((matrix_size, matrix_size), 0, int)
+
+    for i in range(n):
+        for j in range(m):
+            central_node_number = i * m + j
+            if j > 0:
+                left_node_number = central_node_number - 1
+                left_distance = distance_data[i][j - 1]
+                adjacency_matrix[central_node_number, left_node_number] = left_distance
+
+            if j < m - 1:
+                right_node_number = central_node_number + 1
+                right_distance = distance_data[i][j + 1]
+                adjacency_matrix[central_node_number, right_node_number] = right_distance
+
+            if i > 0:
+                up_node_number = central_node_number - m
+                up_distance = distance_data[i - 1][j]
+                adjacency_matrix[central_node_number, up_node_number] = up_distance
+
+            if i < n - 1:
+                down_node_number = central_node_number + m
+                down_distance = distance_data[i + 1][j]
+                adjacency_matrix[central_node_number, down_node_number] = down_distance
+
+    return adjacency_matrix
+
+
+def create_path_matrix(adjacency_matrix: "np.ndarray") -> "np.ndarray":
+    matrix_size = len(adjacency_matrix)
+    path_matrix = np.zeros(adjacency_matrix.shape, int)
+
+    # initialize
+    for i in range(matrix_size):
+        for j in range(matrix_size):
+            path_matrix[i, j] = i
+            if i != j and (adjacency_matrix[i, j] == 0):
+                path_matrix[i, j] = MINIMAL
+                adjacency_matrix[i, j] = MAXIMAL
+    # build
+    for k in range(matrix_size):
+        for i in range(matrix_size):
+            for j in range(matrix_size):
+                if adjacency_matrix[i, j] > adjacency_matrix[i, k] + adjacency_matrix[k, j]:
+                    adjacency_matrix[i, j] = adjacency_matrix[i, k] + adjacency_matrix[k, j]
+                    path_matrix[i, j] = path_matrix[k, j]
+
+    return path_matrix
+
+
+def ConstructPath(p, i, j):
+    i, j = int(i), int(j)
+    if i == j:
+        print(i, )
+    elif p[i, j] == MINIMAL:
+        print(i, '-', j)
+    else:
+        ConstructPath(p, i, p[i, j])
+        print(j, )
+
+
+test_data_2 = [
+    [3, 101, 1, 101, 101],
+    [2, 101, 1, 1, 1],
+    [2, 1, 4, 101, 3]
+]
+
+start_node = (0, 0)
+end_node = (2, 4)
+
+
+def main():
+    a_matrix = create_adjacency_matrix(test_data_2)
+    print(a_matrix)
+    p_matrix = create_path_matrix(a_matrix)
+    print(p_matrix)
+
+    ConstructPath(p_matrix, 0, 14)
+
+
+if __name__ == "__main__":
+    main()
